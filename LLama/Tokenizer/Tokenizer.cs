9160ef585a3ebb62b315ccee0852d01c0679f826
unknown <yrezehi@gmail.com>
2023-11-20 08:09:42 -0800

    add: native configuration

---
+++ LLama/Tokenizer/Tokenizer.cs
@@ -0,0 +1,15 @@
+﻿namespace LLama.Tokenizer
+{
+    public class Tokenizer
+    {
+        private readonly static string DEFAULT_TOKENIZER_PATH = "tokenizer/tokenizer.bin";
+
+        public Tokenizer() {
+            if (File.Exists(DEFAULT_TOKENIZER_PATH))
+            {
+                throw new FileNotFoundException("Couldn't find the tokenizer at: " + DEFAULT_TOKENIZER_PATH);
+            }
+
+        }
+    }
+}
