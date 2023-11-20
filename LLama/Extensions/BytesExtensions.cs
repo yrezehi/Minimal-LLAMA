76013df7ddfd2839cda872c8e3b71558d4f6e58f
unknown <yrezehi@gmail.com>
2023-11-20 06:25:04 -0800

    setup: bytes.ext

---
+++ LLama/Extensions/BytesExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LLama.Extensions
+{
+    public static class BytesExtensions
+    {
+        public static long GetBytesSize(this object @object) =>
+            Marshal.SizeOf(@object);
+    }
+}
