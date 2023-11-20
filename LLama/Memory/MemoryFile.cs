7996386f595a402e6558c68c038b35d0c52c0b1c
unknown <yrezehi@gmail.com>
2023-11-20 06:05:17 -0800

    setup: memoryfile

---
+++ LLama/Memory/MemoryFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
+
+namespace LLama.Memory
+{
+    public class MemoryFile
+    {
+        private readonly MemoryMappedFile MappedFile;
+
+        private MemoryFile(string modelPath)
+        {
+            if (File.Exists(modelPath))
+            {
+                throw new FileNotFoundException("Couldn't find the file at: " + modelPath);
+            }
+
+            var fileStream = this.GetStream(modelPath);
+
+            MappedFile = this.GetMappedFile(fileStream);
+        }
+
+        private FileStream GetStream(string modelPath) =>
+            new FileStream(modelPath, FileMode.Open, FileAccess.Read);
+
+        private MemoryMappedFile GetMappedFile(FileStream stream) =>
+            MemoryMappedFile.CreateFromFile(stream, null, stream.Length, MemoryMappedFileAccess.Read, HandleInheritability.None, false);
+    }
+}
