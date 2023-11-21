3231665d0d84ca9cc13579c917e9bace84fcd2bf
unknown <yrezehi@gmail.com>
2023-11-21 05:16:26 -0800

    sync

---
+++ LLama/Prompts/Prompts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLama.Prompts
+{
+    public class Prompts
+    {
+        public string Prompt;
+
+        public int[]? PromptTokens;
+
+        public Prompts(string prompt) =>
+            (Prompt, PromptTokens) = (prompt, new int[prompt.Length]);
+       
+        public static Prompts Create(string prompt) =>
+            new Prompts(prompt);
+    }
+}
