ecc3cbde900cb5480c7cc60f8ec1812ef1d48406
unknown <yrezehi@gmail.com>
2023-11-20 17:48:22 -0800

    add: loadVocab

--- LLama/Tokenizer/Tokenizer.cs
+++ LLama/Tokenizer/Tokenizer.cs
@@ -1,12 +1,12 @@
-﻿namespace LLama.Tokenizer
+﻿using System.Text;
+
+namespace LLama.Tokenizer
 {
     public class Tokenizer
     {
         private readonly static string DEFAULT_TOKENIZER_PATH = "tokenizer/tokenizer.bin";
 
-        private readonly FileStream FileStream;
         private int MaxLength;
-
         private int VocabSize;
 
         private readonly string[] Vocab;
@@ -19,7 +19,6 @@
                 throw new FileNotFoundException("Couldn't find the tokenizer at: " + DEFAULT_TOKENIZER_PATH);
             }
 
-            FileStream = this.GetStream();
             VocabSize = vocabSize;
 
             Vocab = new string[VocabSize];
@@ -28,6 +27,24 @@
 
         private FileStream GetStream() =>
             new FileStream(DEFAULT_TOKENIZER_PATH, FileMode.Open, FileAccess.Read);
+
+        private void loadVocab()
+        {
+            using FileStream fileStream = this.GetStream();
+            using BinaryReader binaryReader = new BinaryReader(fileStream);
+            
+            for(int index = 0; index < VocabSize; index++)
+            {
+                Vocab[index] = binaryReader.ReadSingle();
+
+                int length = binaryReader.ReadInt32();
+
+                Span<byte> stackBuffer = stackalloc byte[length];
+                _ = binaryReader.Read(stackBuffer);
+
+                Vocab[index] = Encoding.UTF8.GetString(stackBuffer);
+            }
+        }
        
         public static Tokenizer Create(int vocabSize) =>
             new Tokenizer(vocabSize);
