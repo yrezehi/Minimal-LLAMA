7c64cdb79fd987677e0178fc780948ed4093bc51
ykrezaihi <ykrezaihi@sfda.gov.sa>
2023-11-21 13:36:44 +0300

    sync

---
+++ LLama/Configuration/InferenceConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace LLama.Configuration
+{
+	public class InferenceConfiguration
+	{
+		public float Temperature = 1.0f;
+		public float Topp = 0.9f;
+		public int Steps = 256;
+		public long Seed = DateTime.UtcNow.Ticks;
+
+		public static InferenceConfiguration Create() =>
+			new InferenceConfiguration();
+
+	}
+}
--- LLama/Prompts/PromptEncoder.cs
+++ LLama/Prompts/PromptEncoder.cs
@@ -13,12 +13,10 @@ namespace LLama.Prompts
 			for (int index = 0; index < prompt.Length; index++)
 			{
 				int vocabIndex = vocab.FindVocab(prompt.ElementAt(index).ToString());
-
 				if (vocabIndex == -1)
 				{
 					throw new ArgumentException("Couldn't find vocab for: " + prompt);
 				}
-
 				tokens[tokenLength] = vocabIndex;
 			}
 		}
