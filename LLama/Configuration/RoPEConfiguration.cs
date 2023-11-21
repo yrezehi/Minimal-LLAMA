e7cbc2afbbf1b15810f41371259661a343564b1b
unknown <yrezehi@gmail.com>
2023-11-21 07:18:50 -0800

    setup: RoPEConfiguration

---
+++ LLama/Configuration/RoPEConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace LLama.Configuration
+{
+    public struct RoPEConfiguration
+    {
+        public float q0;
+        public float q1;
+
+        public float k0;
+        public float k1;
+
+        public float fcr;
+        public float fci;
+    }
+}
