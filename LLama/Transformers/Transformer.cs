b9b3e5f38b94ba055e7e4ecdeac476646e68b860
unknown <yrezehi@gmail.com>
2023-11-22 07:51:55 -0800

    sync: trans

--- LLama/Transformers/Transformer.cs
+++ LLama/Transformers/Transformer.cs
@@ -16,7 +16,9 @@ namespace LLama.Transformers
         {
             int headSize = configuration.dim / configuration.n_heads;
 
-            for (int index = 0; index < 10; index++)
+            Array.Copy(weights.token_embedding_table, token * configuration.dim, state.x, 0, configuration.dim);
+
+            for (int index = 0; index < configuration.n_layers; index++)
             {
                 RMSNorm.Normailize(new ArraySegment<float>(), new float[1], new float[1], 10);
 
@@ -25,23 +27,47 @@ namespace LLama.Transformers
                 Matmul.Multiple(new float[1], new float[2], new ArraySegment<float>(), 0, 2);
 
                 // RoPE relative positional encoding: complex-valued rotate q and k by freq_cis in each head
-                for (int i = 0; i < 10; i += 2)
+                for (int currentIndex = 0; index < 10; index += 2)
                 {
                     RoPE RoPE = RoPE.Empty();
 
-                    RoPE.q0 = state.q[i];
-                    RoPE.q1 = state.q[i + 1];
-                    RoPE.k0 = state.k[i];
-                    RoPE.k1 = state.k[i + 1];
-                    RoPE.fcr = weights.freq_cis_real[position * headSize / 2 + i % headSize / 2];
-                    RoPE.fci = weights.freq_cis_imag[position * headSize / 2 + i % headSize / 2];
+                    RoPE.q0 = state.q[currentIndex];
+                    RoPE.q1 = state.q[currentIndex + 1];
+                    RoPE.k0 = state.k[currentIndex];
+                    RoPE.k1 = state.k[currentIndex + 1];
+                    RoPE.fcr = weights.freq_cis_real[position * headSize / 2 + currentIndex % headSize / 2];
+                    RoPE.fci = weights.freq_cis_imag[position * headSize / 2 + currentIndex % headSize / 2];
 
-                    state.q[i] = RoPE.q0 * RoPE.fcr - RoPE.q1 * RoPE.fci;
-                    state.q[i + 1] = RoPE.q0 * RoPE.fci + RoPE.q1 * RoPE.fcr;
-                    state.k[i] = RoPE.k0 * RoPE.fcr - RoPE.k1 * RoPE.fci;
-                    state.k[i + 1] = RoPE.k0 * RoPE.fci + RoPE.k1 * RoPE.fcr;
+                    state.q[currentIndex] = RoPE.q0 * RoPE.fcr - RoPE.q1 * RoPE.fci;
+                    state.q[currentIndex + 1] = RoPE.q0 * RoPE.fci + RoPE.q1 * RoPE.fcr;
+                    state.k[currentIndex] = RoPE.k0 * RoPE.fcr - RoPE.k1 * RoPE.fci;
+                    state.k[currentIndex + 1] = RoPE. k0 * RoPE.fci + RoPE.k1 * RoPE.fcr;
                 }
 
+                int loff = index * configuration.seq_len * configuration.dim;
+
+                Array.Copy(state.k, 0, state.key_cache, loff + position * configuration.dim, configuration.dim);
+                Array.Copy(state.v, 0, state.value_cache, loff + position * configuration.dim, configuration.dim);
+
+                Parallel.For(0, configuration.n_heads, head =>
+                {
+                int qOffset = head * headSize;
+
+                int attOffset = head * configuration.seq_len;
+
+                for (int currentIndex = 0; currentIndex <= position; currentIndex++)
+                {
+                    int keyCacheOffset = loff + currentIndex * configuration.dim + head * headSize;
+
+                    float score = 0.0f;
+
+                    for (int currentPosition = 0; currentPosition < headSize; currentPosition++)
+                        score += state.q[currentPosition + qOffset] * state.key_cache[currentPosition + keyCacheOffset];
+
+                    score /= (float)Math.Sqrt(headSize);
+
+                    state.att[currentIndex + attOffset] = score;
+                });
             }
         }
     }
