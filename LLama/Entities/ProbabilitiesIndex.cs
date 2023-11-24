e83593ccad194c04c4c3a256569724a12ac3d3cd
unknown <yrezehi@gmail.com>
2023-11-24 01:55:47 -0800

    milestone: working

--- LLama/Entities/ProbabilitiesIndex.cs
+++ LLama/Entities/ProbabilitiesIndex.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace LLama.Entities
 {
-    public class ProbabilitiesIndex
+    public struct ProbabilitiesIndex
     {
         public float Probability;
         public int Index;
