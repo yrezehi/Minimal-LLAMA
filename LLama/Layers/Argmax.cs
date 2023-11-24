bf49d0493e9241c0158d4cb41b4ee2e609e216ba
unknown <yrezehi@gmail.com>
2023-11-23 20:57:40 -0800

    add: Argmax

---
+++ LLama/Layers/Argmax.cs
@@ -0,0 +1,22 @@
+﻿namespace LLama.Layers
+{
+    public static class Argmax
+    {
+        public static int Maximum(float[] probabilities, int vocabSize)
+        {
+            int maxIndex = 0;
+            float maxProbability = probabilities[0];
+
+            for (int index = 1; index < vocabSize; index++)
+            {
+                if (probabilities[index] > maxProbability)
+                {
+                    maxIndex = index;
+                    maxProbability = probabilities[index];
+                }
+            }
+
+            return maxIndex;
+        }
+    }
+}
